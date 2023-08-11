a63f84396cc7a5d42504e3562912f14c3c7c04c8
Andy Li <andy@onthewings.net>
2023-08-11 21:45:01 +0100

    reproduce the url encoding issue

---
+++ app.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')
+
+const app = fastify()
+app.get('*', (request, reply) => reply.send("request.url: " + request.url))
+
+if (require.main === module) {
+  app.listen({ port: 3000 }, (err) => {
+    if (err) console.error(err)
+    console.log('server listening on 3000')
+  })
+} else {
+  module.exports = app
+}
